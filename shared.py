be936e79f7136eaaca4496351dc0475820567d67
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2023-05-10 10:44:09 -0400

    add RF5 stats

--- shared.py
+++ shared.py
@@ -20,7 +20,7 @@ def get_first_of_month_utc(unix_time):
 ITEM_TYPE_CATEGORY_WEARABLE = 0
 UPDATE_TIME = get_midnight_utc_today()
 WEI_PER_ETH = 10 ** 18
-USE_CACHE = False
+USE_CACHE = True
 TRAIT_NAMES = ['NRG', 'AGG', 'SPK', 'BRN', 'EYS', 'EYC']
 SLOT_POSITION_NAMES = ['Body', 'Face', 'Eyes', 'Head', 'Hand L', 'Hand R', 'Pet', 'BG']
 RARITY_SCORE_MODIFIERS = { 1: 'common', 2: 'uncommon', 5: 'rare', 10: 'legendary', 20: 'mythical', 50: 'godlike'  }
@@ -32,7 +32,7 @@ END_TIME = UPDATE_TIME
 START_TIME = get_first_of_month_utc(END_TIME - TIME_RANGE_SECONDS)
 QUERY_TIME_INTERVAL = 30 * SECONDS_IN_A_DAY # break up the query into 30 day chunks
 EXCLUDE_WEARABLE_IDS = [0, 162] # exclude the void and Miami Shirt (never minted)
-RARITY_FARMING_BLOCKS = [14082019, 20633780, 25806269, 31770753] # adjusted blocks for subgraph error with rf szn 2-3 20633778, 25806267
+RARITY_FARMING_BLOCKS = [14082019, 20633780, 25806269, 31770753, 39284410] # adjusted blocks for subgraph error with rf szn 2-3 20633778, 25806267
 ITABLE_DOM_LONG = "lftipr"
 ITABLE_DOM_SHORT = "tr"
 GHST_COLOR = 'magenta'
