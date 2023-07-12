6d376126596f09ecb94b539033e51835ae72c489
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2023-07-12 15:01:38 -0400

    add forge subgraph

--- utils/subgraph.py
+++ utils/subgraph.py
@@ -4,6 +4,7 @@ import math
 from subgraph.query import SubgraphQuery
 
 AAVEGOTCHI_CORE_MATIC = "https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/api"
+FORGE_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/sudeepb02/aavegotchi-forge-subgraph"
 
 def get_subgraph_result_df(result_obj, id_index=True):
     df = pd.DataFrame(pd.json_normalize(result_obj))
@@ -13,6 +14,9 @@ def get_subgraph_result_df(result_obj, id_index=True):
 
 def get_core_matic_query(query_str):
     return SubgraphQuery(AAVEGOTCHI_CORE_MATIC, query_str)
+
+def get_forge_query(query_str):
+    return SubgraphQuery(FORGE_SUBGRAPH_URL, query_str)
 
 def round_time_to_nearest_minutes(minutes=1, precise_time=time.time()):
     return math.floor(precise_time / (60 * minutes)) * (60 * minutes)
